c1824d546362e1dbb8a559d7f59daaf964806a5e
Clément Durand <clement.durand@bordeaux-inp.fr>
2025-10-01 16:27:25 +0200

    trop bien séparer 2 fichiers

--- adaptation rasp/dynamics.py
+++ adaptation rasp/dynamics.py
@@ -1,8 +1,9 @@
 import numpy as np
 import time
 import turtle
-import threading
 #import pypot.dynamixel
+
+f = open("positions.txt", "w+")
 
 R_roue = 51.5 / 1000
 d = 135 / 1000
@@ -10,7 +11,6 @@ x = 0
 y = 0
 theta = 0
 last_time = time.time()
-f = open("positions.txt", "w+")
 
 def deg2rad(deg):
     return deg * np.pi / 180
@@ -64,26 +64,6 @@ def detect_path(wl, wr, dxl_io, dxl1=1, dxl2=2):
     theta += w*dt
     
     f.write(str(x)+","+str(y)+","+str(theta)+"\n")
-
-def list_pos_to_draw():
-    fenetre = turtle.Screen()
-    fenetre.title("Tortue Python 🐢")
-    fenetre.bgcolor("lightblue")
-
-    # Créer la tortue
-    t = turtle.Turtle()
-    t.shape("turtle")
-    t.color("green")
-    t.speed(3)
-    t.clear()
-    t.pendown()
-    for line in f.readlines():
-        x_str, y_str, theta_str = line.strip().split(",")
-        x, y, theta = float(x_str), float(y_str), float(theta_str)
-        t.goto(x*1000, y*1000)
-    t.penup()
-    turtle.done()
-
 
 
 
