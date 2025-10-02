84b07ef2118cf315c1781179a8e8d6db49c682fa
Aymeric Cassard <aymeric.cassard@laposte.net>
2025-10-02 09:25:09 +0200

    photo.py

---
+++ adaptation rasp/photo.py
@@ -0,0 +1,6 @@
+import cv2 as cv2
+
+webcam = cv2.VideoCapture(0)
+ret, frame = webcam.read()
+small = cv2.resize(frame, (0,0), fx=0.10, fy=0.10, interpolation=cv2.INTER_AREA)
+cv2.imwrite("photo.jpg", small, [cv2.IMWRITE_JPEG_QUALITY, 20])
