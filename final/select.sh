9fd77de3dc6eb843ab3f272fcbcd4f6c4138b7b4
Clément Durand <clement.durand@bordeaux-inp.fr>
2025-10-03 14:23:35 +0200

    commit

--- final/select.sh
+++ final/select.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap '' SIGINT   # le Bash ignore CTRL+C
 
 # Vérifier qu'un argument a été fourni
 if [ -z "$1" ]; then
@@ -12,14 +13,17 @@ case "$NOM" in
     "suivi_ligne")
         echo "Lancement du suivi de ligne"
         python3 suivi_ligne.py follow_line
+        python3 cleaner.py
         ;;
     "free_ride")
         echo "Lancement du suivi de ligne"
         python3 suivi_ligne.py free_ride
+        python3 cleaner.py
         ;;
     "goto")
         echo "Lancement du goto"
         python3 goto.py
+        python3 cleaner.py
         ;;
     "affichage_plot")
         echo "Affichage dans un plot en cours..."
